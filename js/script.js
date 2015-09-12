11b284c59ed815e7ceba792b8a61a68c976f0b67
JP Lacy <lacyjpr@gmail.com>
2015-09-12 12:09:59 -0700

    Comment out hide/toggle divs code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //borrowed from http://designgala.com/how-to-expand-collapse-toggle-div-layer-using-jquery/
-jQuery(document).ready(function() {
-  jQuery(".concept-content").hide();
-  jQuery(".concept-title").click(function()
-  {
-    jQuery(this).next(".concept-content").slideToggle(10);
-  });
-});
+// jQuery(document).ready(function() {
+//   jQuery(".wrapper").hide();
+//   jQuery(".lesson-header").click(function()
+//   {
+//     jQuery(this).next(".wrapper").slideToggle(10);
+//   });
+// });
